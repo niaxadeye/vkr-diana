6bd78a75ec8f1a72fc5debd1ce7c83cf8ba36474
MainPC\Erick <niaxadeye@gmail.com>
2026-05-27 16:07:14 +0300

    change adress conc

--- merch-store/client/src/entities/delivery-address/lib/formatDeliveryAddress.ts
+++ merch-store/client/src/entities/delivery-address/lib/formatDeliveryAddress.ts
@@ -3,11 +3,13 @@ import type { DeliveryAddress } from "../model/deliveryAddress.types";
 export function formatDeliveryAddress(address: DeliveryAddress) {
     const parts = [
         address.city,
-        address.street,
-        `д. ${address.house}`,
+        address.street ? address.street : null,
+        address.house ? `д. ${address.house}` : null,
         address.apartment ? `кв./офис ${address.apartment}` : null,
         address.entrance ? `подъезд ${address.entrance}` : null,
         address.floor ? `этаж ${address.floor}` : null,
+        address.cdekPvzName ? `ПВЗ СДЕК ${address.cdekPvzName}` : null,
+        address.cdekPvzAddress ? address.cdekPvzAddress : null,
     ].filter(Boolean);
 
     return parts.join(", ");
